a56a93119c5a8d0bf77601b5257f38cf1fba34fa
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-12-07 05:18:04 -0500

    Update coinspec.js

--- src/lib/coinspec.js
+++ src/lib/coinspec.js
@@ -1,7 +1,7 @@
 
 
 let network = {
-    messagePrefix: '\x19Beetlecoin Signed Message:\n',
+    messagePrefix: '\x19Help The Homeless Coin Signed Message:\n',
     bip32: {
         public: 0x0488b21e,
         private: 0x0488ade4
